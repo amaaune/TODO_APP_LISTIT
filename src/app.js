a466bf405ea169066e55d8655784b73549c5459c
aamaury <amaury.aune@ynov.com>
2026-05-13 13:25:25 +0200

    [FIX] app.js

--- src/app.js
+++ src/app.js
@@ -1,18 +1,12 @@
 require('dotenv').config();
+const cors = require('cors');
 const express = require('express');
 const path = require('path');
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(express.static('public'));
-
-const cors = require('cors');
-app.use(cors());
-
-// Servir index.html pour la racine
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../public/index.html'));
-});
 
 app.get('/api/health', (req, res) => {
   res.json({ status: "ok", uptime: process.uptime() });
